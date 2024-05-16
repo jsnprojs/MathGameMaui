14b92fef42deef92bd23a84703d31b6cfed325fe
kang yi fan <jason3631061@gmail.com>
2024-05-15 22:06:14 -0700

    Add project files.

---
+++ MathGameMaui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MathGameMaui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MathGameMaui/MainPage.xaml.cs
@@ -0,0 +1,8 @@
+﻿namespace MathGameMaui
+{
+    public partial class MainPage : ContentPage
+    {
+
+    }
+
+}
